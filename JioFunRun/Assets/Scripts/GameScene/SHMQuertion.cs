6966711309262c8111dea9e5a30f45a7a901271b
manikarna <97171564+manikarna19@users.noreply.github.com>
2022-04-22 16:28:59 +0530

    16:28

---
+++ JioFunRun/Assets/Scripts/GameScene/SHMQuertion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHMQuertion : MonoBehaviour
+{
+    public float Speed;
+    public float X, Y, Z;
+    public float LL, UL;
+    public bool isClock = true;
+    public float angle;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isClock)
+        {
+           
+        }
+        else
+        {
+
+        }
+    }
+}
