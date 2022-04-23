ea875766b6aa87c47a06c4cabb923509cd820917
manikarna <97171564+manikarna19@users.noreply.github.com>
2022-04-23 11:49:01 +0530

    11:48

---
+++ JioFunRun/Assets/Scripts/Level2/Torque1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Torque1 : MonoBehaviour
+{
+
+    private Vector3 initPos, initRot;
+    public float t = 1;
+    public Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initPos = transform.position;
+        initRot = transform.eulerAngles;
+        StartCoroutine(ApplyTorque());
+
+    }
+
+    private IEnumerator ApplyTorque()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(t);
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            transform.position = initPos;
+            transform.eulerAngles = initRot;
+
+        }
+    }
+}
